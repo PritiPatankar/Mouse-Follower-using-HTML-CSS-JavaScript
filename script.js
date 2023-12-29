f47ce6eacadb59a2a41e8bad1b08ae15f2075ab6
PritiPatankar <115872293+PritiPatankar@users.noreply.github.com>
2023-12-29 15:29:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-//get location of mouser using mousemove event listner on window
-//set these locations to the circle
+//Logic ->
+//get the location of the mouse using details of mousemove event listener on window.
+//set these locations to the circle(mouser follower).
 
 var circle = document.querySelector('#circle');
 
 window.addEventListener('mousemove', function(details){
     let xValue = details.clientX;
     let yValue = details.clientY;
-
-    this.setTimeout(() => {
+    
+//use setTimeout function to slower mouser follower than original mouse ->
+    setTimeout(() => {
         circle.style.top = `${yValue}px`;
         circle.style.left = `${xValue}px`;
     }, 100);
